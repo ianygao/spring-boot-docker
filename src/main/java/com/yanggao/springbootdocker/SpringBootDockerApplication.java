3334406232f848b2f0d0cc7b2d298ee0c0a96352
Yang Gao <ygaowhu@outlook.com>
2022-11-03 01:06:55 -0400

    Initial commit

---
+++ src/main/java/com/yanggao/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,25 @@
+package com.yanggao.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+	@RestController
+	static class BootController {
+		@GetMapping("/{name}")
+		public ResponseEntity<String> greeting(@PathVariable(value = "name") String name) {
+			return ResponseEntity.ok("Hey " + name + ", how are you?");
+		}
+	}
+
+}
---
+++ src/test/java/com/yanggao/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanggao.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
